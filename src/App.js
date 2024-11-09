8dafe20be8ef7383f09eca1cfaa765f05d9a18b2
6303217506 <vanamdhanushnetha@gmail.com>
2024-11-09 16:50:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import BookingForm from './components/BookingForm';
+import './src/assests/styles/main.css'
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <BookingForm />
+    </Router>
+  );
+};
+
+export default App;
